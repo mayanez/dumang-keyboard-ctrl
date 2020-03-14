2b4ffea6369cdfd37b473dcbda914743cfc2c262
Miguel Arroyo <miguel@arroyo.me>
2020-03-13 19:33:01 -0700

    Update setup.py + version

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import setup
+import setuptools
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -8,7 +8,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
-setup(
+setuptools.setup(
     name="dumang_ctrl",
     version="0.0.1",
     description="Dumang DK6 Tools",
@@ -16,14 +16,14 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/mayanez/",
     author="Miguel A. Arroyo",
-    author_email="email@arroyo.me",
-    license="MIT",
+    author_email="dumang@arroyo.me",
+    license="GPLv3",
     classifiers=[
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=["dumang_ctrl", "dumang_ctrl.dumang"],
+    packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=["docopt", "pyudev", "hidapi", "PyQt5"],
     entry_points={
