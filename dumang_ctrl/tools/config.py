5b03103b07230d694ef1d391ad86c09cbc10e246
Miguel A. Arroyo <miguel@arroyo.me>
2024-03-14 16:10:10 -0700

    fix: Load CFG

--- dumang_ctrl/tools/config.py
+++ dumang_ctrl/tools/config.py
@@ -1,9 +1,9 @@
-import logging
-import sys
-from collections import OrderedDict
-
 import click
+import logging
+import signal
+import sys
 import yaml
+from collections import OrderedDict
 
 import dumang_ctrl as pkginfo
 from dumang_ctrl.dumang.common import *
@@ -25,6 +25,7 @@ CTX_THREADS_KEY = "THREADS"
 
 
 class NestedDict(OrderedDict):
+
     def __missing__(self, key):
         self[key] = NestedDict()
         return self[key]
@@ -55,66 +56,82 @@ def find_key_by_serial(kbd, serial):
     return None
 
 
-def configure_key(b, key, cfg):
+def configure_key(b, key, cfg_key):
     layer_keycodes = {}
-    for l in cfg:
-        if not l.startswith(YAML_LABEL_LAYER_PREFIX):
+    for layer in cfg_key:
+        if not layer.startswith(YAML_LABEL_LAYER_PREFIX):
             continue
-        layer = int(l.split(YAML_LABEL_LAYER_PREFIX[-1])[1])
-        layer_keycodes[layer] = Keycode.fromstr(cfg[l])
+        layer_int = int(layer.split(YAML_LABEL_LAYER_PREFIX[-1])[1])
+        layer_keycodes[layer_int] = Keycode.fromstr(cfg_key[layer])
 
-    b.put(KeyConfigurePacket(key, layer_keycodes))
+    if (key.layer_keycodes != layer_keycodes):
+        logger.debug(
+            f"Configuring DKM serial {cfg_key[YAML_LABEL_SERIAL]} to {layer_keycodes}"
+        )
+        b.put(KeyConfigurePacket(key, layer_keycodes))
+    else:
+        logger.debug(
+            f"DKM serial {cfg_key[YAML_LABEL_SERIAL]} already properly configured"
+        )
 
-    macro = cfg.get(YAML_LABEL_MACRO)
-    if macro:
+    macro = cfg_key.get(YAML_LABEL_MACRO)
+    if macro and (key.macro != macro):
+        logger.debug(
+            f"Configuring DKM serial {cfg_key[YAML_LABEL_SERIAL]} macro")
         idx = 0
         for m in macro:
             b.put(
-                MacroConfigurePacket(
-                    key, idx, MacroType.fromstr(m["type"]), Keycode.fromstr(m["key"]), int(m["delay_ms"])
-                )
-            )
+                MacroConfigurePacket(key, idx,
+                                     MacroType.fromstr(m[YAML_LABEL_TYPE]),
+                                     Keycode.fromstr(m[YAML_LABEL_KEY]),
+                                     int(m[YAML_LABEL_DELAY_MS])))
             idx += 1
         b.put(MacroConfigurePacket(key, idx, MacroType(0), Keycode(0), 0))
 
 
-def configure_keys(kbd_serial, cfg, kbds):
+def configure_keys(cfg_kbd, kbds):
     n = 0
-    for k in cfg:
-        key_serial = cfg[k].get(YAML_LABEL_SERIAL, None)
+    for k in cfg_kbd[YAML_LABEL_KEYS]:
+        cfg_key = k[YAML_LABEL_KEY]
+
+        key_serial = cfg_key.get(YAML_LABEL_SERIAL, None)
         if key_serial is None:
             logger.error(f"DKM config without serial {k}")
             sys.exit(1)
-        b = None
-        key = None
+
+        board = None
         for kbd in kbds:
             key = find_key_by_serial(kbd, key_serial)
             if key is not None:
-                b = kbd
+                board = kbd
                 break
         if key is None:
             logger.error(f"DKM with serial {key_serial} not found")
             sys.exit(1)
-        if b.serial != kbd_serial:
+
+        cfg_kbd_serial = cfg_kbd[YAML_LABEL_SERIAL]
+        if board.serial != cfg_kbd_serial:
             logger.warning(
-                f"DKM with serial {key_serial} found on a different board. Maybe moved from board {kbd_serial} to {b.serial} ?"
+                f"DKM with serial {key_serial} found on a different board. Maybe moved from board {cfg_kbd_serial} to {b.serial} ?"
             )
 
-        configure_key(b, key, cfg[k])
+        configure_key(board, key, cfg_key)
         n += 1
     return n
 
 
 def configure_boards(cfg, kbds):
     n = 0
-    for kbd in cfg:
-        n += configure_keys(cfg[kbd][YAML_LABEL_SERIAL], cfg[kbd][YAML_LABEL_KEYS], kbds)
+    for cfg_kbd in cfg:
+        n += configure_keys(cfg_kbd[YAML_LABEL_BOARD], kbds)
     return n
 
 
 @click.group(help="Configuration Tool", invoke_without_command=True)
 @click.option("--verbose", help="Enable Verbose Logging", is_flag=True)
-@click.option("--very-verbose", help="Enable Very Verbose Logging", is_flag=True)
+@click.option("--very-verbose",
+              help="Enable Very Verbose Logging",
+              is_flag=True)
 @click.option("--version", help="Print Version", is_flag=True)
 @click.pass_context
 def cli(ctx, verbose, very_verbose, version):
@@ -154,7 +171,12 @@ def dump(ctx):
     n = 0
     cfg_yml = []
     for _i, kbd in enumerate(ctx.obj[CTX_KEYBOARDS_KEY]):
-        cfg_board = {YAML_LABEL_BOARD: {YAML_LABEL_SERIAL: kbd.serial, YAML_LABEL_KEYS: []}}
+        cfg_board = {
+            YAML_LABEL_BOARD: {
+                YAML_LABEL_SERIAL: kbd.serial,
+                YAML_LABEL_KEYS: []
+            }
+        }
 
         cfg_keys = cfg_board[YAML_LABEL_BOARD][YAML_LABEL_KEYS]
         for _, dkm in kbd.configured_keys.items():
@@ -164,23 +186,28 @@ def dump(ctx):
             for l, kc in dkm.layer_keycodes.items():
                 # NOTE: Use str() here to get ANY of the valid
                 # aliases should a keycode have them.
-                cfg_key[YAML_LABEL_KEY][f"{YAML_LABEL_LAYER_PREFIX}{l}"] = str(kc)
+                cfg_key[YAML_LABEL_KEY][f"{YAML_LABEL_LAYER_PREFIX}{l}"] = str(
+                    kc)
             if dkm.macro:
-                cfg_key[YAML_LABEL_KEY][YAML_LABEL_MACRO] = [
-                    {
-                        YAML_LABEL_TYPE: str(m.type),
-                        YAML_LABEL_KEY: str(m.keycode),
-                        YAML_LABEL_DELAY_MS: m.delay,
-                    }
-                    for m in dkm.macro
-                ]
+                cfg_key[YAML_LABEL_KEY][YAML_LABEL_MACRO] = [{
+                    YAML_LABEL_TYPE:
+                    str(m.type),
+                    YAML_LABEL_KEY:
+                    str(m.keycode),
+                    YAML_LABEL_DELAY_MS:
+                    m.delay,
+                } for m in dkm.macro]
             cfg_keys.append(cfg_key)
             n += 1
 
         cfg_yml.append(cfg_board)
         kbd.kill_threads()
 
-    yaml.dump(cfg_yml, sys.stdout, allow_unicode=True, default_flow_style=False, sort_keys=False)
+    yaml.dump(cfg_yml,
+              sys.stdout,
+              allow_unicode=True,
+              default_flow_style=False,
+              sort_keys=False)
     logger.info(f"Dumped {n} keys.")
 
     for t in ctx.obj[CTX_THREADS_KEY]:
